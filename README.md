d6fdd9a75b431df9700eea966fa10f19861a4b39
EQUENOS <63377939+EQUENOS-2@users.noreply.github.com>
2026-07-20 21:39:57 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Obsidian Predictor
 A python script for TMC players that predicts possible obsidian formations in a perimeter.
+
+## Why
+But world eaters have AND-gates! What's the point of this script if I can just fix my WE as soon as it gets stuck?
+
+If you're making a large perimeter in an ocean, kelp can really ruin your day. Even if you're using a kelp-proof WE, if just one sweeper gets stuck it's no longer kelp-proof. In modern versions (1.21.5+) each loaded chunk random-ticks, so when you log on to finally fix the sweeper, you may witness the entire layer flooded from multiple kelp growths that occured. Therefore, you really **don't want** you world eater to stop until it removes all oceans. In order to guarantee that, this script might be very handy.
+
+## How to use
+1. Download [obi_predict.py](obi_predict.py) and [airlike_blocks.json](airlike_blocks.json) and place them in the same directory. Launching the script requires **Python 3.9+** and the `litemapy` library, which can be installed by running `pip install litemapy` in command prompt.
+2. Make a litematica of the region you want to scan. Optionally, you can specify `Corner 1` of your selection at the end of the file name, separating coordinates by space. If you do that, the script will parse that corner. Example: `Terrain Scan 42 -64 42.litematic`.
+3. Launch the script. It will ask you to choose a litematica file. If the file name contains corner coordinates, it will parse them right away. If not, it will ask for coordinates of `Corner 1` of your selection.
+4. The script will generate xaero waypoints and put them in the folder it's located in. You can then copy them and paste in the corresponding xaero file.
+
+## How to interpret waypoints
+Red waypoints indicate lava layers that should be covered. They are grouped, meaning that a single waypoint on a large lava lake indicates that the **entire** surface of that lake should be covered. Some lava lakes have red waypoints on multiple depths. That simply means that it should be drained.
+
+Black waypoints indicate blast-resistant blocks. These waypoints are also grouped. For example, a nether portal will be indicated with just one black waypoint.
---
+++ obi_predict.py
@@ -0,0 +1,371 @@
+import json
+from collections import deque
+from enum import IntEnum
+from itertools import product
+from math import ceil, log
+from tkinter import filedialog
+
+import nbtlib
+import numpy as np
+from litemapy import BlockState
+from nbtlib import LongArray
+
+WATER: str = "minecraft:water"
+LAVA: str = "minecraft:lava"
+
+TOUGH_BLOCKS: set[str] = {
+    "minecraft:obsidian",
+    "minecraft:vault",
+    "minecraft:trial_spawner",
+    "minecraft:crying_obsidian",
+    "minecraft:ender_chest",
+    "minecraft:reinforced_deepslate",
+    "minecraft:end_portal_frame",
+    "minecraft:anvil",
+    "minecraft:chipped_anvil",
+    "minecraft:damaged_anvil",
+    "minecraft:enchanting_table",
+    "minecraft:ancient_debris",
+    "minecraft:netherite_block",
+    "minecraft:heavy_core",
+    "minecraft:respawn_anchor",
+}
+
+FRAGILE_BLOCKS: set[str] = {
+    "minecraft:glow_lichen",
+    "minecraft:mangrove_leaves",
+    "minecraft:small_dripleaf",
+    "minecraft:big_dripleaf",
+    "minecraft:big_dripleaf_stem",
+    "minecraft:pointed_dripstone",
+}
+
+AIRLIKE: set[str]
+
+with open("airlike_blocks.json", "r") as fp:
+    AIRLIKE = set(json.load(fp))
+
+
+class Color(IntEnum):
+    BLACK = 0
+    DARK_BLUE = 1
+    DARK_GREEN = 2
+    DARK_AQUA = 3
+    DARK_RED = 4
+    DARK_PURPLE = 5
+    GOLD = 6
+    GRAY = 7
+    DARK_GRAY = 8
+    BLUE = 9
+    GREEN = 10
+    AQUA = 11
+    RED = 12
+    LIGHT_PURPLE = 13
+    YELLOW = 14
+    WHITE = 15
+
+
+def get_level(block: BlockState) -> int:
+    return int(dict(block.properties())["level"])
+
+
+def is_blast_resistant(block: BlockState) -> bool:
+    if dict(block.properties()).get("waterlogged") == "true":
+        return block.id not in FRAGILE_BLOCKS
+    return block.id in TOUGH_BLOCKS
+
+
+def parse_area_origin(path: str) -> tuple[int, int, int]:
+    try:
+        filename = path.rsplit("/", maxsplit=1)[-1].rsplit("\\", maxsplit=1)[-1]
+        name = filename.rsplit(".", maxsplit=1)[0]
+        return tuple(map(int, name.split()[-3:]))  # type: ignore
+    except Exception:
+        return tuple(
+            map(int, input("Input Corner 1 of your area selection: ").split())
+        )  # type: ignore
+
+
+def format_waypoint(
+    x: int, y: int, z: int, color: Color, name: str, short_name: str | None = None
+) -> str:
+    if short_name is None:
+        short_name = name[:2]
+    return f"waypoint:{name}:{short_name.upper()}:{x}:{y}:{z}:{color}:false:0:gui.xaero_default:false:0:0:false"
+
+
+class LitematicaBitArrayProxy:
+    M: int = (1 << 64) - 1
+    nbits: int
+    raw_array: LongArray
+    _mask: int
+
+    def __init__(self, arr: LongArray, nbits: int) -> None:
+        self.nbits = nbits
+        self.raw_array = arr
+        self._mask = (1 << nbits) - 1
+
+    def __getitem__(self, index) -> int:
+        start_offset = index * self.nbits
+        start_arr_index = start_offset >> 6
+        end_arr_index = ((index + 1) * self.nbits - 1) >> 6
+        start_bit_offset = start_offset & 0x3F  # last 6 bits
+
+        if start_arr_index == end_arr_index:
+            return (
+                self.raw_array[start_arr_index] & self.M
+            ) >> start_bit_offset & self._mask
+        else:
+            end_offset = 64 - start_bit_offset
+            val = (self.raw_array[start_arr_index] & self.M) >> start_bit_offset | (
+                self.raw_array[end_arr_index] & self.M
+            ) << end_offset
+            return val & self._mask
+
+
+class RegionProxy:
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        length: int,
+        palette: list[BlockState],
+        bit_array: LitematicaBitArrayProxy,
+    ):
+        self._width: int = width
+        self._height: int = height
+        self._length: int = length
+
+        self.width: int = abs(width)
+        self.height: int = abs(height)
+        self.length: int = abs(length)
+
+        self.palette: list[BlockState] = palette
+        self.bit_array: LitematicaBitArrayProxy = bit_array
+
+    def __getitem__(self, point: tuple[int, int, int]) -> BlockState:
+        x, y, z = point
+        assert (
+            0 <= x < self.width and 0 <= y < self.height and 0 <= z < self.length
+        ), f"Index {point} is out of range; Shape={(self.width, self.height, self.length)}"
+        ind = (y * self.width * self.length) + z * self.width + x
+        return self.palette[self.bit_array[ind]]
+
+    @staticmethod
+    def from_file(path: str) -> "RegionProxy":
+        schem = nbtlib.File.load(path, True)
+        nbt = next(iter(schem["Regions"].values()))
+
+        palette: list[BlockState] = []
+        for block_nbt in nbt["BlockStatePalette"]:
+            block = BlockState.from_nbt(block_nbt)  # type: ignore
+            palette.append(block)
+
+        nbits = max(ceil(log(len(palette), 2)), 2)
+        size = nbt["Size"]
+        return RegionProxy(
+            int(size["x"]),
+            int(size["y"]),
+            int(size["z"]),
+            palette,
+            LitematicaBitArrayProxy(nbt["BlockStates"], nbits),
+        )
+
+
+class ObsidianPredictor:
+    def __init__(self, x0: int, y0: int, z0: int, region: RegionProxy):
+        self.region: RegionProxy = region
+
+        self.origin_x: int = min(x0, x0 + region._width + 1)
+        self.origin_y: int = min(y0, y0 + region._height + 1)
+        self.origin_z: int = min(z0, z0 + region._length + 1)
+
+        self.size_x: int = region.width
+        self.size_y: int = region.height
+        self.size_z: int = region.length
+
+        self.layer_matrix: np.ndarray = np.full(
+            (self.size_x, self.size_z), 9, dtype=np.int16
+        )
+        self.upper_marked_lava: set[tuple[int, int]] = set()
+        self.lower_marked_lava: set[tuple[int, int]] = set()
+        self.blast_resistant_markers: set[tuple[int, int, int]] = set()
+        self.markers: list[tuple[int, int, int]] = []
+
+    def to_world_coorinates(self, x: int, y: int, z: int) -> tuple[int, int, int]:
+        return self.origin_x + x, self.origin_y + y, self.origin_z + z
+
+    def simulate_water(self, x, y, z, water_level, check_collisions=True) -> None:
+        """Simulates liquid propagation from a given water block, updating `layer_matrix`
+        with new water values."""
+        to_process = deque()
+        to_process.append((x, z, water_level))
+        terminal_water_level = 7
+
+        while to_process:
+            x, z, water_level = to_process.popleft()
+            if (
+                not (0 <= x < self.size_x)
+                or not (0 <= z < self.size_z)
+                or water_level >= self.layer_matrix[x, z]
+                or (check_collisions and self.region[x, y, z].id not in AIRLIKE)
+            ):
+                continue
+
+            self.layer_matrix[x, z] = water_level
+            # this is needed to stop propagation
+            if water_level == terminal_water_level:
+                continue
+            next_level = (water_level + 1) % 8
+            # water flows sideways only if it has a supporting block or it is a source
+            if y > 0 and (
+                water_level == 0 or self.region[x, y - 1, z].id not in AIRLIKE | {WATER}
+            ):
+                to_process.append((x + 1, z, next_level))
+                to_process.append((x - 1, z, next_level))
+                to_process.append((x, z + 1, next_level))
+                to_process.append((x, z - 1, next_level))
+            elif check_collisions:
+                # It is sufficient to stop processing for water of higher level than this.
+                # This is the level at which the given stream encountered a height drop.
+                terminal_water_level = water_level
+
+    def mark_lava_component(
+        self, x: int, y: int, z: int, cache: set[tuple[int, int]]
+    ) -> None:
+        """Marks all adjacent lava sources at the given y-level"""
+        to_process = deque()
+        to_process.append((x, z))
+
+        while to_process:
+            x, z = to_process.popleft()
+            if (
+                (x, z) in cache
+                or not (0 <= x < self.size_x)
+                or not (0 <= z < self.size_z)
+            ):
+                continue
+            block = self.region[x, y, z]
+            if block.id == LAVA and get_level(block) == 0:
+                cache.add((x, z))
+                to_process.append((x + 1, z))
+                to_process.append((x - 1, z))
+                to_process.append((x, z + 1))
+                to_process.append((x, z - 1))
+
+    def try_place_marker(self, x: int, y: int, z: int) -> None:
+        """Checks whether a lava source belongs to a group.
+        If not, places a marker and saves a new group."""
+        if (x, z) in self.upper_marked_lava:
+            return
+        self.mark_lava_component(x, y, z, self.upper_marked_lava)
+        self.markers.append((x, y, z))
+
+    def process_layer(self, y: int) -> None:
+        """Inflates all water sources at the given y level,
+        processes downwards flow from previous layers, marks flooded lava sources,
+        scans for blast resistant blocks."""
+        sources = []
+        flows = []
+
+        for x, z in product(range(self.size_x), range(self.size_z)):
+            block = self.region[x, y, z]
+            if block.id == WATER and get_level(block) == 0:
+                # we will inflate this as if the eater destroyed all surroundings
+                sources.append((x, y, z))
+            elif block.id in AIRLIKE and self.layer_matrix[x, z] < 9:
+                # we will inherit the flow from directly above
+                flows.append((x, y, z))
+            elif (
+                block.id == LAVA
+                and self.layer_matrix[x, z] < 9
+                and get_level(block) == 0
+            ):
+                # mark lava if it has (simulated) water directly above
+                self.try_place_marker(x, y, z)
+            elif is_blast_resistant(block):
+                self.blast_resistant_markers.add((x, y, z))
+            self.layer_matrix[x, z] = 9
+        # layer_matrix is flushed; simulate water
+        for x, y, z in sources:
+            self.simulate_water(x, y, z, 0, check_collisions=False)
+        for x, y, z in flows:
+            self.simulate_water(x, y, z, 8, check_collisions=True)
+
+    def group_waypoints(
+        self, center: tuple[int, int, int]
+    ) -> set[tuple[int, int, int]]:
+        group: set[tuple[int, int, int]] = set()
+        to_process = deque()
+        to_process.append(center)
+
+        while to_process:
+            x, y, z = to_process.popleft()
+            if (
+                not (
+                    0 <= x < self.size_x
+                    and 0 <= y < self.size_y
+                    and 0 <= z < self.size_z
+                )
+                or (x, y, z) not in self.blast_resistant_markers
+                or (x, y, z) in group
+            ):
+                continue
+            group.add((x, y, z))
+            to_process.append((x + 1, y, z))
+            to_process.append((x - 1, y, z))
+            to_process.append((x, y + 1, z))
+            to_process.append((x, y - 1, z))
+            to_process.append((x, y, z + 1))
+            to_process.append((x, y, z - 1))
+
+        for point in group:
+            self.blast_resistant_markers.discard(point)
+
+        return group
+
+    def get_blast_resistant_markers(self) -> list[tuple[int, int, int]]:
+        markers = []
+        while self.blast_resistant_markers:
+            point = next(iter(self.blast_resistant_markers))
+            markers.append(point)
+            self.group_waypoints(point)
+        return markers
+
+    def save_waypoints(self, path: str) -> None:
+        blastres_markers = self.get_blast_resistant_markers()
+
+        with open(path, "w") as fp:
+            for x, y, z in self.markers:
+                fp.write(
+                    f"{format_waypoint(*self.to_world_coorinates(x, y, z), Color.RED, 'X')}\n"
+                )
+
+            for x, y, z in blastres_markers:
+                fp.write(
+                    f"{format_waypoint(*self.to_world_coorinates(x, y, z), Color.BLACK, 'X')}\n"
+                )
+
+    def run(self) -> None:
+        for y in range(self.size_y - 1, -1, -1):
+            print(f"{(self.size_y - y) / self.size_y * 100:.2f} %")
+            self.process_layer(y)
+            self.upper_marked_lava.clear()
+
+
+def main() -> None:
+    path = filedialog.askopenfilename(filetypes=[("Litematica", "*.litematic")])
+    if not path:
+        return print("Terminating...")
+
+    region = RegionProxy.from_file(path)
+    x0, y0, z0 = parse_area_origin(path)
+
+    predictor = ObsidianPredictor(x0, y0, z0, region)
+    print(f"Removing layers from '{path}' and simulating liquids...")
+    predictor.run()
+    predictor.save_waypoints("waypoints.txt")  # mw$default_1.txt
+
+
+if __name__ == "__main__":
+    main()
